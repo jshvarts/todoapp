a1602fb8f234be1da6d952b929d521e54c9a03a5
Alex Hart <alex.hart@redspace.com>
2017-02-08 17:39:54 -0400

    Add Minimal Conductor Code for Main Activity

--- app/src/main/kotlin/com/exallium/todoapp/MainActivity.kt
+++ app/src/main/kotlin/com/exallium/todoapp/MainActivity.kt
@@ -1,10 +1,36 @@
 package com.exallium.todoapp
 
 import android.app.Activity
+import android.os.Bundle
+import android.view.ViewGroup
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Router
 
 /**
  * Single Activity for Application
  */
 class MainActivity : Activity() {
 
+    @BindView(R.id.conductor_container)
+    lateinit var container: ViewGroup
+
+    lateinit var router: Router
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        ButterKnife.bind(this)
+        router = Conductor.attachRouter(this, container, savedInstanceState)
+        if (!router.hasRootController()) {
+            // TODO -- Show Starting Screen
+        }
+    }
+
+    override fun onBackPressed() {
+        if (!router.handleBack()) {
+            super.onBackPressed()
+        }
+    }
 }
