24c07426497cc71d944022b55809b8ec736ad3a4
Alex Hart <alex.hart@redspace.com>
2017-02-09 14:13:19 -0400

    Variable name change

--- app/src/main/kotlin/com/exallium/todoapp/allnotes/AllNotesPresenter.kt
+++ app/src/main/kotlin/com/exallium/todoapp/allnotes/AllNotesPresenter.kt
@@ -5,7 +5,7 @@ import com.exallium.todoapp.mvp.BasePresenter
 /**
  * Presenter for AllNotes Screen
  */
-class AllNotesPresenter(private val allNotesView: AllNotesView) : BasePresenter<AllNotesView>(allNotesView) {
+class AllNotesPresenter(private val view: AllNotesView) : BasePresenter<AllNotesView>(view) {
 
     override fun onViewCreated() {
         // once the view is created and bound, we can hand it the adapter
