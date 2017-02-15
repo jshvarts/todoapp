84356580860e2b1d53b2d5e29564fda239ae7bbe
Alex Hart <alex.hart@redspace.com>
2017-02-14 22:26:49 -0400

    Condense presenter

--- app/src/main/kotlin/com/exallium/todoapp/allnotes/AllNotesPresenter.kt
+++ app/src/main/kotlin/com/exallium/todoapp/allnotes/AllNotesPresenter.kt
@@ -1,5 +1,6 @@
 package com.exallium.todoapp.allnotes
 
+import com.exallium.todoapp.database.Note
 import com.exallium.todoapp.mvp.BasePresenter
 
 /**
@@ -8,17 +9,16 @@ import com.exallium.todoapp.mvp.BasePresenter
 class AllNotesPresenter(private val view: AllNotesView,
                         private val adapter: AllNotesAdapter) : BasePresenter<AllNotesView>(view) {
 
+    private val showNoteSubscriber = { note: Note? ->
+        view.showSingleNote(note?.id)
+    }
+
     override fun onViewCreated() {
         view.setAdapter(adapter)
         adapter.requestUpdate()
 
-        compositeSubscription.add(adapter.noteClicks().subscribe {
-            view.showSingleNote(it.id)
-        })
-
-        compositeSubscription.add(view.addNoteClicks().subscribe {
-            view.showSingleNote()
-        })
+        compositeSubscription.add(adapter.noteClicks().subscribe(showNoteSubscriber))
+        compositeSubscription.add(view.addNoteClicks().map { null }.subscribe(showNoteSubscriber))
     }
 
     override fun onViewDestroyed() {
