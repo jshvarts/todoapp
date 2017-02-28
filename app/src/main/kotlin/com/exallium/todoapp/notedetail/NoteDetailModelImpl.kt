5b17122912fcf61a0bb89bb0bdf81b05fa9cbda5
James Shvarts <jshvarts@gmail.com>
2017-02-27 21:55:42 -0500

    19: add tests for NoteDetailModelImpl

--- app/src/main/kotlin/com/exallium/todoapp/notedetail/NoteDetailModelImpl.kt
+++ app/src/main/kotlin/com/exallium/todoapp/notedetail/NoteDetailModelImpl.kt
@@ -5,6 +5,6 @@ import com.exallium.todoapp.repository.Repository
 /**
  * Implementation for Note Detail model.
  */
-class NoteDetailModelImpl(val repository: Repository) : NoteDetailModel {
+class NoteDetailModelImpl(private val repository: Repository) : NoteDetailModel {
     override fun getNoteById(id: String) = repository.getNoteById(id)
 }
---
+++ app/src/test/kotlin/com/exallium/todoapp/notedetail/NoteDetailModelImplTest.kt
@@ -0,0 +1,38 @@
+package com.exallium.todoapp.notedetail
+
+import com.exallium.todoapp.repository.Repository
+import com.nhaarman.mockito_kotlin.verify
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Answers
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+/**
+ * Unit testing for {@link NoteDetailModelImpl}.
+ */
+class NoteDetailModelImplTest {
+    @InjectMocks
+    private lateinit var testSubject: NoteDetailModelImpl
+
+    @Mock(answer = Answers.RETURNS_MOCKS)
+    private lateinit var repository: Repository
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun getNoteById_retrievesNoteByIdFromRepository() {
+        // GIVEN
+        val noteId: String = "test note id"
+
+        // WHEN
+        testSubject.getNoteById(noteId)
+
+        // THEN
+        verify(repository).getNoteById(noteId)
+    }
+}
