7d6c7453e475512e5f4a1124ef1a350a39375a84
James Shvarts <jshvarts@gmail.com>
2017-03-08 21:31:10 -0500

    47: use butterknife for binding error messaging

--- app/src/main/kotlin/com/exallium/todoapp/editnote/EditNoteViewImpl.kt
+++ app/src/main/kotlin/com/exallium/todoapp/editnote/EditNoteViewImpl.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import butterknife.BindString
 import butterknife.BindView
 import com.exallium.todoapp.R
 import com.exallium.todoapp.app.TodoApp
@@ -24,13 +25,19 @@ class EditNoteViewImpl(val bundle: Bundle) : BaseViewImpl<EditNoteView, EditNote
     lateinit var noteTitleEditText: EditText
 
     @BindView(R.id.edit_note_body)
-    lateinit var noteBodyTextView: EditText
+    lateinit var noteBodyEditText: EditText
 
     @BindView(R.id.edit_note_cancel_button)
     lateinit var cancelEditNote: View
 
     @BindView(R.id.edit_note_save_button)
     lateinit var saveNote: View
+
+    @BindString(R.string.invalid_note_title_error)
+    lateinit var noteTitleError: String
+
+    @BindString(R.string.invalid_note_body_error)
+    lateinit var noteBodyError: String
 
     override fun getComponent(): EditNoteComponent = DaggerEditNoteComponent.builder()
             .todoAppComponent(TodoApp.component)
@@ -41,7 +48,7 @@ class EditNoteViewImpl(val bundle: Bundle) : BaseViewImpl<EditNoteView, EditNote
 
     override fun setNoteData(note: Note) {
         noteTitleEditText.setText(note.title)
-        noteBodyTextView.setText(note.body)
+        noteBodyEditText.setText(note.body)
     }
 
     override fun cancelEditNoteClicks(): Observable<Unit> = cancelEditNote.clicks()
@@ -50,7 +57,7 @@ class EditNoteViewImpl(val bundle: Bundle) : BaseViewImpl<EditNoteView, EditNote
 
     override fun titleTextChanges(): Observable<CharSequence> = noteTitleEditText.textChanges()
 
-    override fun bodyTextChanges(): Observable<CharSequence> = noteBodyTextView.textChanges()
+    override fun bodyTextChanges(): Observable<CharSequence> = noteBodyEditText.textChanges()
 
     override fun showNewNoteDetail(args: Bundle) {
         view?.hideKeyboard(applicationContext?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
@@ -62,7 +69,7 @@ class EditNoteViewImpl(val bundle: Bundle) : BaseViewImpl<EditNoteView, EditNote
     }
 
     override fun getNewNoteBody(): String {
-        return noteBodyTextView.text.toString()
+        return noteBodyEditText.text.toString()
     }
 
     override fun showUnableToLoadNoteError() {
@@ -74,18 +81,14 @@ class EditNoteViewImpl(val bundle: Bundle) : BaseViewImpl<EditNoteView, EditNote
     }
 
     override fun showInvalidNoteTitleError() {
-        noteTitleEditText.error = resources!!.getString(R.string.invalid_note_title_error)
+        noteTitleEditText.error = noteTitleError
     }
 
     override fun showInvalidNoteBodyError() {
-        noteBodyTextView.error = resources!!.getString(R.string.invalid_note_body_error)
+        noteBodyEditText.error = noteBodyError
     }
 
     override fun toggleSubmit(enable: Boolean) {
         saveNote.isEnabled = enable
-    }
-
-    private fun View.hideKeyboard(inputMethodManager: InputMethodManager) {
-        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
     }
 }
