50da93d1ed653fef47626485c619f6213347b9b4
James Shvarts <jshvarts@gmail.com>
2017-03-14 22:10:45 -0400

    27: add tests for EditNotePresenter.setupGetNoteDetailSubscription()

--- app/src/test/kotlin/com/exallium/todoapp/editnote/EditNotePresenterTest.kt
+++ app/src/test/kotlin/com/exallium/todoapp/editnote/EditNotePresenterTest.kt
@@ -2,7 +2,9 @@ package com.exallium.todoapp.editnote
 
 import android.os.Bundle
 import com.exallium.todoapp.R
+import com.exallium.todoapp.entities.Note
 import com.exallium.todoapp.repository.IdFactory
+import com.exallium.todoapp.repository.RepositoryException
 import com.exallium.todoapp.screenbundle.ScreenBundleHelper
 import com.nhaarman.mockito_kotlin.*
 import org.junit.Before
@@ -11,31 +13,32 @@ import org.mockito.Answers
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import rx.Single
 
 /**
  * Unit testing for {@link EditNotePresenter}
  */
 class EditNotePresenterTest {
 
-    private val TEST_NOTE_ID_STRING = "test note id"
+    val TEST_NOTE_ID_STRING = "test note id"
 
     @InjectMocks
-    private lateinit var testSubject: EditNotePresenter
+    lateinit var testSubject: EditNotePresenter
 
     @Mock(answer = Answers.RETURNS_MOCKS)
-    private lateinit var view: EditNoteView
+    lateinit var view: EditNoteView
 
     @Mock(answer = Answers.RETURNS_MOCKS)
-    private lateinit var model: EditNoteModel
+    lateinit var model: EditNoteModel
 
     @Mock(answer = Answers.RETURNS_MOCKS)
-    private lateinit var screenBundleHelper: ScreenBundleHelper
+    lateinit var screenBundleHelper: ScreenBundleHelper
 
     @Mock(answer = Answers.RETURNS_MOCKS)
-    private lateinit var idFactory: IdFactory
+    lateinit var idFactory: IdFactory
 
     @Mock
-    private lateinit var bundle: Bundle
+    lateinit var bundle: Bundle
 
     @Before
     fun setUp() {
@@ -151,4 +154,31 @@ class EditNotePresenterTest {
         // THEN
         verify(testSubject).setupTextChangedSubscription()
     }
+
+    @Test
+    fun setupGetNoteDetailSubscription_whenNoteIdIsSet_andNoteIsFound_setsNoteData() {
+        // GIVEN
+        val note = getTestNote()
+        whenever(model.getNote(TEST_NOTE_ID_STRING)).thenReturn(Single.just(note))
+
+        // WHEN
+        testSubject.setupGetNoteDetailSubscription(TEST_NOTE_ID_STRING)
+
+        // THEN
+        verify(view).setNoteData(note)
+    }
+
+    @Test
+    fun setupGetNoteDetailSubscription_whenNoteIdIsSet_andNoteIsNotFound_showsUnableToLoadNoteError() {
+        // GIVEN
+        whenever(model.getNote(TEST_NOTE_ID_STRING)).thenReturn(Single.error(RepositoryException("")))
+
+        // WHEN
+        testSubject.setupGetNoteDetailSubscription(TEST_NOTE_ID_STRING)
+
+        // THEN
+        verify(view).showUnableToLoadNoteError()
+    }
+
+    private fun getTestNote() = Note(TEST_NOTE_ID_STRING, "", "", 0, 0)
 }
